b56b6d23e7b728104dd4a2e4164fb5b9127a3a23
josiah <918780@dpsk12.net>
2022-01-31 11:06:28 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 const app = express();
+
+app.use(bodyParser.json)
 
 const bannedFiles = []
 const port = 3000
 
+var openGames = []
+
+var  games =[]
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname+'/index.html')
 });
+
+app.get('/game', (req, res) => {
+  res.sendFile(__dirname+'/game/game.html')
+});
+
+app.post('/game', (req, res) => {
+req.body
+})
 
 app.get('*', (req, res) => {
 	if(!bannedFiles.includes(req.url)){
